b80328365902a3737b3349292b44f8c4aabac373
Andi Rückauer <arueckauer@gmail.com>
2022-06-21 10:11:38 +0200

    Adds implementation of material change event request handler

--- src/Api/Event/MaterialChange/RequestHandler.php
+++ src/Api/Event/MaterialChange/RequestHandler.php
@@ -4,15 +4,39 @@ declare(strict_types=1);
 
 namespace Api\Event\MaterialChange;
 
+use ApiConnector\Material\DataToCommerce\Transferrer;
 use Laminas\Diactoros\Response\EmptyResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
+
+use function is_string;
 
 class RequestHandler implements RequestHandlerInterface
 {
+    public function __construct(
+        private readonly Transferrer $transferrer
+    ) {
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        return new EmptyResponse(501);
+        $skuCode = $request->getAttribute('sku-code');
+
+        if (
+            ! is_string($skuCode)
+            || $skuCode === ''
+        ) {
+            return new EmptyResponse(400);
+        }
+
+        try {
+            ($this->transferrer)($skuCode);
+        } catch (Throwable) {
+            return new EmptyResponse(500);
+        }
+
+        return new EmptyResponse();
     }
 }

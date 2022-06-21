ae5614b1329071f16671491fdbad14bb295d9f76
Andi Rückauer <arueckauer@gmail.com>
2022-06-21 09:30:15 +0200

    Adds static ctor fromResponse

--- src/Sap/Material/Dto.php
+++ src/Sap/Material/Dto.php
@@ -4,11 +4,32 @@ declare(strict_types=1);
 
 namespace Sap\Material;
 
+use Psr\Http\Message\ResponseInterface;
+
+use function json_decode;
+
+use const JSON_THROW_ON_ERROR;
+
 class Dto
 {
     public function __construct(
         public readonly string $sku,
         public readonly string $name,
     ) {
+    }
+
+    public static function fromResponse(ResponseInterface $response): Dto
+    {
+        $data = json_decode(
+            $response->getBody()->getContents(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+
+        return new self(
+            $data['sku'],
+            $data['name'],
+        );
     }
 }

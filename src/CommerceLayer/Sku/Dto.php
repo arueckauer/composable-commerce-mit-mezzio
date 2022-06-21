5bffa2f51553a58b6d13af76703288905449430d
Andi Rückauer <arueckauer@gmail.com>
2022-06-21 09:12:00 +0200

    Adds CommerceLayer Sku DTO

---
+++ src/CommerceLayer/Sku/Dto.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CommerceLayer\Sku;
+
+use Psr\Http\Message\ResponseInterface;
+
+use function json_decode;
+
+use const JSON_THROW_ON_ERROR;
+
+class Dto
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly string $code,
+        public readonly string $name,
+    ) {
+    }
+
+    public static function fromResponse(ResponseInterface $response): self
+    {
+        $data = json_decode(
+            $response->getBody()->getContents(),
+            true,
+            512,
+            JSON_THROW_ON_ERROR
+        );
+
+        return new self(
+            $data['data']['id'],
+            $data['data']['attributes']['code'],
+            $data['data']['attributes']['name'],
+        );
+    }
+}
